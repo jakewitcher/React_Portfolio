2a70cfab3af5e9a9c96eaad529b695f3565d3c1a
Jake Witcher <jjwitcher09@gmail.com>
2019-01-01 19:17:20 -0500

    add constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+export default {
+  contact: {
+    EMAIL: process.env.REACT_APP_EMAIL || "jakewitcher@codefornerds.com"
+  },
+  social: {
+    GITHUB_URL:
+      process.env.REACT_APP_GITHUB_URL || "https://github.com/jakewitcher",
+    LINKEDIN_URL:
+      process.env.REACT_APP_LINKEDIN_URL ||
+      "https://www.linkedin.com/in/jake-witcher-162190176/",
+    TWITTER_URL:
+      process.env.REACT_APP_TWITTER_URL || "https://twitter.com/codefornerds",
+    WORDPRESS_URL:
+      process.env.REACT_APP_WORDPRESS_URL || "http://www.codefornerds.com"
+  }
+};
