79b1963da8aed9ba33b10b4f3a3c28d1f20b6bde
Jake Witcher <jjwitcher09@gmail.com>
2019-01-02 22:15:52 -0500

    make changes to animation speed, fix bug with project animation and navbar, correct spelling error on home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ import Title from "./Title";
 
 const Home = () => {
   const subText =
-    "Hi, my name is Jake and I'm a software developer who loves crafting web applications with React. I like to spend my free time learning new programming languages, patterns, and frameworks and building my own side projects. I'm also a big fan of colaborating with other talented developers.";
+    "Hi, my name is Jake and I'm a software developer who loves crafting web applications with React. I like to spend my free time learning new programming languages, patterns, and frameworks and building my own side projects. I'm also a big fan of collaborating with other talented developers.";
   return (
     <div className="home">
       <div className="home__text-container">
