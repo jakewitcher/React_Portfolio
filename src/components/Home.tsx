af2334d0685084fab68e7801ea725b4012a596fe
Jake Witcher <jjwitcher09@gmail.com>
2019-01-12 20:09:17 -0500

    finish mobile styling for home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ import Title from "./Title";
 
 const Home = () => {
   const subText =
-    "Hi, my name is Jake and I'm a software developer who loves crafting web applications with React. I like to spend my free time learning new programming languages, patterns, and frameworks and building my own side projects. I'm also a big fan of collaborating with other talented developers.";
+    "Hi, my name is Jake and I'm a software developer. I love using functional programming concepts in JavaScript and building web applications with React. You can find articles I've written on these subjects and more on my blog, 'Code for Nerds.'";
   return (
     <div className="home">
       <div className="home__text-container">
@@ -14,13 +14,14 @@ const Home = () => {
           parentComponent="home"
           subTitle="Front-End Developer"
         />
-        <Text subText={subText} parentComponent="home" />
+        <Text subText={subText} parentComponent="home" mobile="" />
       </div>
       <Image
         src="/images/headshot.jpg"
         alt="Headshot of Jake Witcher"
         parentComponent="home"
       />
+      <Text subText={subText} parentComponent="home" mobile="--mobile" />
     </div>
   );
 };
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,12 +3,13 @@ import * as React from "react";
 interface ITextProps {
   subText: string;
   parentComponent: string;
+  mobile: string;
 }
 
 const Text = (props: ITextProps) => {
-  const { subText, parentComponent: component } = props;
+  const { subText, parentComponent: component, mobile } = props;
   return (
-    <div className={`${component}__text-box`}>
+    <div className={`${component}__text-box${mobile}`}>
       <p className={`${component}__text`}>{subText}</p>
     </div>
   );
