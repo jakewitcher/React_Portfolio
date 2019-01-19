d05ec7a97cb72805e6f765ad047fe9c5666d3cfd
Jake Witcher <jjwitcher09@gmail.com>
2019-01-19 15:31:17 -0500

    add link to blog in home page text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,8 +4,6 @@ import Text from "./Text";
 import Title from "./Title";
 
 const Home = () => {
-  const subText =
-    "Hi, my name is Jake and I'm a software developer. I love using functional programming concepts in JavaScript and building web applications with React. You can find articles I've written on these subjects and more on my blog, 'Code for Nerds.'";
   return (
     <div className="home">
       <div className="home__text-container">
@@ -14,14 +12,14 @@ const Home = () => {
           parentComponent="home"
           subTitle="Front-End Developer"
         />
-        <Text subText={subText} parentComponent="home" mobile="" />
+        <Text parentComponent="home" mobile="" />
       </div>
       <Image
         src="/images/headshot.jpg"
         alt="Headshot of Jake Witcher"
         parentComponent="home"
       />
-      <Text subText={subText} parentComponent="home" mobile="--mobile" />
+      <Text parentComponent="home" mobile="--mobile" />
     </div>
   );
 };
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,16 +1,31 @@
 import * as React from "react";
+import utils from "../utils/constants";
 
 interface ITextProps {
-  subText: string;
   parentComponent: string;
   mobile: string;
 }
 
+const { WORDPRESS_URL } = utils.social;
+
 const Text = (props: ITextProps) => {
-  const { subText, parentComponent: component, mobile } = props;
+  const { parentComponent: component, mobile } = props;
   return (
     <div className={`${component}__text-box${mobile}`}>
-      <p className={`${component}__text`}>{subText}</p>
+      <p className={`${component}__text`}>
+        Hi, my name is Jake and I'm a software developer. I love using
+        functional programming concepts in JavaScript and building web
+        applications with React. You can find articles I've written on these
+        subjects and more on my blog,{" "}
+        <a
+          href={WORDPRESS_URL}
+          rel="noopener noreferrer"
+          className={`${component}__text-link`}
+          target="_blank"
+        >
+          "Code for Nerds."
+        </a>
+      </p>
     </div>
   );
 };
