ad6e47c9ce95d2bf7b6b2818b9894db048ad5b77
Jake Witcher <jjwitcher09@gmail.com>
2019-01-24 19:39:05 -0500

    temp fix navbar onClick sections

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,39 +27,23 @@ class Navbar extends React.Component {
         </a>
         <i onClick={this.toggleNav} className="fas fa-bars navbar__hamburger" />
         <div className="navbar__link-box">
-          <a onClick={this.toggleNav} className={this.state.linkClass} href="#">
+          <a className={this.state.linkClass} href="#">
             <p className="navbar__link-text">Home</p>
           </a>
 
-          <a
-            onClick={this.toggleNav}
-            className={this.state.linkClass}
-            href="#recent-works"
-          >
+          <a className={this.state.linkClass} href="#recent-works">
             <p className="navbar__link-text">Recent Work</p>
           </a>
 
-          <a
-            onClick={this.toggleNav}
-            className={this.state.linkClass}
-            href="#skills"
-          >
+          <a className={this.state.linkClass} href="#skills">
             <p className="navbar__link-text">Skills</p>
           </a>
 
-          <a
-            onClick={this.toggleNav}
-            className={this.state.linkClass}
-            href="#recommendations"
-          >
+          <a className={this.state.linkClass} href="#recommendations">
             <p className="navbar__link-text">Recommendations</p>
           </a>
 
-          <a
-            onClick={this.toggleNav}
-            className={this.state.linkClass}
-            href="#contact"
-          >
+          <a className={this.state.linkClass} href="#contact">
             <p className="navbar__link-text">Contact</p>
           </a>
         </div>
